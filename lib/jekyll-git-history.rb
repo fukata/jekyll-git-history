1a001519861063ecd11caad305cfb63e40d06f5d
fukata <tatsuya.fukata@gmail.com>
2020-12-07 11:05:52 +0900

    Implemented render git histories

--- lib/jekyll-git-history.rb
+++ lib/jekyll-git-history.rb
@@ -7,7 +7,38 @@ class GitHistory < Liquid::Tag
     @text = text
   end
 
-  def render(content)
+  def render(context)
+    site = context.registers[:site].config
+
+    # Get git histories by git command
+    filepath = context.registers[:page]["path"]
+    cmd = "git log --format='%h %cI %s' #{filepath}"
+    output = `#{cmd}`
+
+    commit_page_url = nil
+    if site['git_history'] && site['git_history']['repo']
+      repo = site['git_history']['repo']
+      # TODO: Support another services
+      commit_page_url = "https://github.com/#{repo}/commit/" 
+    end
+
+    inner_html_list = [] 
+    output.split("\n").each do |line|
+      line.strip!
+
+      # Convert short hash to link
+      if commit_page_url && commit_page_url.size > 0
+        line = line.gsub(/^([0-9a-z]{7})/) { %Q{<a href="#{commit_page_url}#{$1}">#{$1}</a>} }
+      end
+
+      inner_html_list.push(%Q{<li class="git-history">#{line}</li>})
+    end
+
+    <<"HTML"
+<ul class="git-histories">
+  #{inner_html_list.join("\n")}
+</ul>
+HTML
   end
 
   Liquid::Template.register_tag "git_history", self
